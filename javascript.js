1fd87523e1eb38667646992b803a507211078688
codingMilad <m3loo@yahoo.com>
2022-08-28 11:08:44 +0200

    added most of the functionality

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let choice = ["rock", "paper", "scissors"];
+  let choice = ['rock', 'paper', 'scissors'];
   return choice[Math.floor(Math.random() * choice.length)];
 }
 let user = 0;
@@ -7,42 +7,48 @@ let pc = 0;
 
 //takes 2 parameters, compares it against each other and decides the winner (or tie).
 function startGame(playerSelection, computerSelection) {
-  let playerChoice = prompt("Please choose between Rock, Paper or Scissors: ");
+  // let playerChoice = prompt('Please choose between Rock, Paper or Scissors: ');
 
-  playerSelection = playerChoice.toLowerCase();
+  // playerSelection = playerChoice.toLowerCase();
+  let div = document.createElement('p');
   computerSelection = getComputerChoice();
 
-  if (playerSelection === "paper" && computerSelection === "paper") {
-    console.log("It`s a tie!");
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("You Lose! Paper beats Rock");
+  if (playerSelection === 'paper' && computerSelection === 'paper') {
+    console.log('It`s a tie!');
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    console.log('You Lose! Paper beats Rock');
     pc += 1;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log("You Win!, Rock beats scissors");
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    console.log('You Win!, Rock beats scissors');
     user += 1;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("You Win!, Rock beats scissors");
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    console.log('You Win!, Rock beats scissors');
     user += 1;
-  } else if (playerSelection === "rock" && computerSelection === "rock") {
-    console.log("It`s a tie!");
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("You Lose! Paper beats Rock");
+  } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    console.log('It`s a tie!');
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    console.log('You Lose! Paper beats Rock');
     pc += 1;
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("You Lose! Paper beats Rock");
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    console.log('You Lose! Paper beats Rock');
     pc += 1;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("You Win!, Rock beats scissors");
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    console.log('You Win!, Rock beats scissors');
     user += 1;
   } else {
-    console.log("It`s a tie!");
+    console.log('It`s a tie!');
   }
 
   console.log(user, pc);
+  div.innerHTML = '${user}-${pc}';
+  const para = document.createElement('p');
+  para.innerText = `${user}-${pc}`;
+  document.body.appendChild(para);
   console.log(playerSelection, computerSelection);
 }
+
 //starts the game 5 times
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 1; i++) {
   getComputerChoice();
   startGame();
   if (i == 4) {
@@ -50,13 +56,28 @@ for (let i = 0; i < 5; i++) {
   }
 }
 
-//takes after the 5 rounds the user and pc and decides if who won or if it is a tie
+// takes after the 5 rounds the user and pc and decides if who won or if it is a tie
 function isFiveRound() {
   if (user > pc) {
-    return "You Won";
+    return 'You Won';
   } else if (pc > user) {
-    return "You Lose!";
+    return 'You Lose!';
   } else {
-    return "It`s a tie!";
+    return 'It`s a tie!';
   }
 }
+
+const rock = document.getElementById('rock');
+rock.addEventListener('click', function (e) {
+  startGame('rock');
+});
+
+const paper = document.getElementById('paper');
+paper.addEventListener('click', function () {
+  startGame('paper');
+});
+
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', function () {
+  startGame('scissors');
+});
